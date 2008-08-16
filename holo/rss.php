92f501348920ff1ce7a17c8ad86b018b458a408f
yifanlu <yifanlu@e0e46c49-be69-4f5a-ad62-21024a331aea>
2008-08-16 15:10:59 +0000

    Fixed rss.php

--- holo/rss.php
+++ holo/rss.php
@@ -1,5 +1,5 @@
 <?php
-include{'core.php')
+include('core.php');
 header("Content-type: text/xml");
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 <rss xmlns:taxo=\"http://purl.org/rss/1.0/modules/taxonomy/\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" version=\"2.0\">";
@@ -10,17 +10,18 @@ echo "
     <description>The latest happenings on ".$shortname." direct to your news reader</description>";
 
 $data = mysql_query("SELECT * FROM cms_news ORDER BY num DESC");
-while($row = mysql_fetch_array($data))
+while($row = mysql_fetch_array($data)){
 
 echo "
     <item>
       <title>".$row[title]."</title>
       <link>".$path."news.php?id=".$row[num]."</link>
-      <description>".$row[story]."</description>
+      <description>".$row[shortstory]."</description>
       <pubDate>".$row[date]."</pubDate>
       <guid isPermaLink=\"true\">".$path."news.php?id=".$row[num]."</guid>
       <dc:date>".$row[date]."</dc:date>
     </item>";
+}
 
 echo "
 </channel>
